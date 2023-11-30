d8f0dfa6a58cb2719430e07fc933bff8a4d3af00
Angelita Rettore de Araujo Zanella <50925433+angelitazanella@users.noreply.github.com>
2023-11-30 16:24:47 -0300

    Add files via upload

---
+++ download/pt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://www.dca.ufrn.br/~viegas/disciplinas/DCA0113/files/Simuladores/821/CiscoPacketTracer_821_Ubuntu_64bit.deb
+
+dpkg -i CiscoPacketTracer_821_Ubuntu_64bit.deb
